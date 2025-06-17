4c63465ee3fff4eedcd05ff977c9a9a6174e05d9
ykylee <144748346+ykylee@users.noreply.github.com>
2025-06-17 16:33:56 +0900

    Add MFC dialog skeleton with open/close buttons

---
+++ ImageDialogApp/stdafx.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#endif
+
+#include "targetver.h"
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+
+#ifndef _AFX_NO_MFC_CONTROLS_IN_DIALOGS
+#include <afxctl.h>
+#endif
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#include <afxdisp.h>        // MFC Automation classes
+#include <afxdtctl.h>       // MFC support for Internet Explorer 4 Common Controls
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>         // MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include <afxcontrolbars.h>
+
